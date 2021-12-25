00a28cea11355aab5021d7e43ff279d3690dee3b
Nish26 <Nfactor26@gmail.com>
2021-12-25 13:13:25 +0530

    Enable client credentials grant flow

--- src/Server/Controllers/AuthorizationController.cs
+++ src/Server/Controllers/AuthorizationController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
+using Microsoft.IdentityModel.Tokens;
 using OpenIddict.Abstractions;
 using OpenIddict.Server.AspNetCore;
 using Pixel.Identity.Provider.Helpers;
@@ -19,6 +20,7 @@ using Pixel.Identity.Shared.Models;
 using Pixel.Identity.Shared.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -474,7 +476,6 @@ namespace Mvc.Server
                 // Returning a SignInResult will ask OpenIddict to issue the appropriate access/identity tokens.
                 return SignIn(principal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
             }
-
             else if (request.IsAuthorizationCodeGrantType() || request.IsDeviceCodeGrantType() || request.IsRefreshTokenGrantType())
             {
                 // Retrieve the claims principal stored in the authorization code/device code/refresh token.
@@ -516,10 +517,65 @@ namespace Mvc.Server
                 // Returning a SignInResult will ask OpenIddict to issue the appropriate access/identity tokens.
                 return SignIn(principal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
             }
+            else if(request.IsClientCredentialsGrantType()) 
+            {
+                // Note: the client credentials are automatically validated by OpenIddict:
+                // if client_id or client_secret are invalid, this action won't be invoked.
 
+                var application = await _applicationManager.FindByClientIdAsync(request.ClientId);
+                if (application == null)
+                {
+                    throw new InvalidOperationException("The application details cannot be found in the database.");
+                }
+
+                // Create a new ClaimsIdentity containing the claims that
+                // will be used to create an id_token, a token or a code.
+                var identity = new ClaimsIdentity(TokenValidationParameters.DefaultAuthenticationType, Claims.Name, Claims.Role);
+
+                // Use the client_id as the subject identifier.
+                identity.AddClaim(Claims.Subject, await _applicationManager.GetClientIdAsync(application),
+                    Destinations.AccessToken, Destinations.IdentityToken);
+
+                identity.AddClaim(Claims.Name, await _applicationManager.GetDisplayNameAsync(application),
+                    Destinations.AccessToken, Destinations.IdentityToken);
+
+                // Note: In the original OAuth 2.0 specification, the client credentials grant
+                // doesn't return an identity token, which is an OpenID Connect concept.
+                //
+                // As a non-standardized extension, OpenIddict allows returning an id_token
+                // to convey information about the client application when the "openid" scope
+                // is granted (i.e specified when calling principal.SetScopes()). When the "openid"
+                // scope is not explicitly set, no identity token is returned to the client application.
+
+                // Set the list of scopes granted to the client application in access_token.
+                var principal = new ClaimsPrincipal(identity);
+                principal.SetScopes(request.GetScopes());
+                principal.SetResources(await _scopeManager.ListResourcesAsync(principal.GetScopes()).ToListAsync());
+
+                foreach (var claim in principal.Claims)
+                {
+                    claim.SetDestinations(GetDestinations(claim));
+                }
+
+                return SignIn(principal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+            }  
+            
             throw new InvalidOperationException("The specified grant type is not supported.");
         }
         #endregion
+
+        private IEnumerable<string> GetDestinations(Claim claim)
+        {
+            // Note: by default, claims are NOT automatically included in the access and identity tokens.
+            // To allow OpenIddict to serialize them, you must attach them a destination, that specifies
+            // whether they should be included in access tokens, in identity tokens or in both.
+
+            return claim.Type switch
+            {
+                Claims.Name or Claims.Subject  => ImmutableArray.Create(Destinations.AccessToken, Destinations.IdentityToken),
+                _ => ImmutableArray.Create(Destinations.AccessToken),
+            };
+        }
 
         private IEnumerable<string> GetDestinations(Claim claim, ClaimsPrincipal principal)
         {
