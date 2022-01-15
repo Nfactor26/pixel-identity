324c8590293a3ff281b89e62a8ef293365505b07
Nish26 <Nfactor26@gmail.com>
2022-01-15 23:05:33 +0530

    Accounts should be locked on subsequent failed login attempts

--- src/Pixel.Identity.Core/Pages/Login.cs
+++ src/Pixel.Identity.Core/Pages/Login.cs
@@ -78,7 +78,7 @@ namespace Pixel.Identity.Core.Pages
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {                  
                     return LocalRedirect(returnUrl);
