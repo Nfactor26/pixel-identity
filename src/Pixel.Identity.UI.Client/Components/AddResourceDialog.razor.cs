aef73d397de2b4b95b0819d37bb09012acd4eb15
Nish26 <Nfactor26@gmail.com>
2022-01-23 23:47:14 +0530

    Refactor dialog components in to code behind

---
+++ src/Pixel.Identity.UI.Client/Components/AddResourceDialog.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pixel.Identity.UI.Client.Components
+{
+    public partial class AddResourceDialog : ComponentBase
+    {
+        string error = null;
+        string resource = string.Empty;
+
+        [CascadingParameter]
+        MudDialogInstance MudDialog { get; set; }
+
+        [Parameter]
+        public IEnumerable<string> ExistingResources { get; set; }
+
+        /// <summary>
+        /// Close the dialog with resource as dialog result
+        /// </summary>
+        void AddNewResource()
+        {
+            if (!ExistingResources.Any(u => u.Equals(resource)))
+            {
+                MudDialog.Close(DialogResult.Ok<string>(resource));
+                return;
+            }
+            error = "Resource is already added.";
+            return;
+        }
+
+        /// <summary>
+        /// Close the dialog without any result
+        /// </summary>
+        void Cancel() => MudDialog.Cancel();
+    }
+}
---
+++ src/Pixel.Identity.UI.Client/Components/AddRoleDialog.razor.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Pixel.Identity.Shared.Request;
+using Pixel.Identity.Shared.ViewModels;
+using Pixel.Identity.UI.Client.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pixel.Identity.UI.Client.Components
+{
+    public partial class AddRoleDialog : ComponentBase
+    {
+        UserRoleViewModel selectedOption;
+
+        [CascadingParameter]
+        MudDialogInstance MudDialog { get; set; }
+
+        [Inject]
+        public ISnackbar SnackBar { get; set; }
+
+        [Inject]
+        public IUserRolesService RolesService { get; set; }
+
+        /// <summary>
+        /// Close the dialog with selectedOption as dialog result
+        /// </summary>
+        void AddRole()
+        {
+            if (null != selectedOption)
+            {
+                MudDialog.Close(DialogResult.Ok<UserRoleViewModel>(selectedOption));
+            }
+        }
+
+        /// <summary>
+        /// Close the dialog without any result
+        /// </summary>
+        void Cancel() => MudDialog.Cancel();
+
+        /// <summary>
+        /// Get the filtered roles for auto complete text box as user types 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private async Task<IEnumerable<UserRoleViewModel>> SearchRoles(string value)
+        {
+            // if text is null or empty, don't return values (drop-down will not open)
+            if (string.IsNullOrEmpty(value))
+                return null;
+            var result = await RolesService.GetRolesAsync(new GetRolesRequest()
+            {
+                CurrentPage = 1,
+                PageSize = 10,
+                RoleFilter = value
+            });
+            return result.Items;
+        }
+    }
+}
