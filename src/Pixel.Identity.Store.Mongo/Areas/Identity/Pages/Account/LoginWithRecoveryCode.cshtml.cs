c7dfaa193e2b70d11b40118e53ca452502155b1e
Nish26 <Nfactor26@gmail.com>
2022-02-06 16:23:51 +0530

    Custom pages for loginwith2fa and loginwithrecovery code

---
+++ src/Pixel.Identity.Store.Mongo/Areas/Identity/Pages/Account/LoginWithRecoveryCode.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Identity;
+using Pixel.Identity.Core.Pages;
+
+namespace Pixel.Identity.Store.Mongo.Account
+{
+    public class LoginWithRecoveryCodeModel : LoginWithRecoveryCodeModel<ApplicationUser>
+    {
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="signInManager"></param>
+        /// <param name="userManager"></param>
+        public LoginWithRecoveryCodeModel(SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager) : base(signInManager, userManager)
+        {
+        }
+    }
+}
