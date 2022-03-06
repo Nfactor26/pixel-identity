1e94c82982ea6af709411bd0c6364ebc29880b18
Nish26 <Nfactor26@gmail.com>
2022-03-06 15:26:02 +0530

    Added exception handling by logging error message while sending an email

--- src/Pixel.Identity.Messenger.Email/EmailSender.cs
+++ src/Pixel.Identity.Messenger.Email/EmailSender.cs
@@ -1,5 +1,6 @@
 ﻿using MailKit.Net.Smtp;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using MimeKit;
 using MimeKit.Text;
 using Pixel.Identity.Core;
@@ -9,26 +10,36 @@ namespace Pixel.Identity.Messenger.Email
     public class EmailSender : IEmailSender
     {
         private readonly IConfiguration configuration;
-
-        public EmailSender(IConfiguration configuration)
+        private readonly ILogger<EmailSender> logger;
+     
+        public EmailSender(IConfiguration configuration, ILogger<EmailSender> logger)
         {
             this.configuration = configuration;
+            this.logger = logger;
         }
 
         public async Task SendEmailAsync(string sendTo, string subject, string htmlMessage)
         {
-            using (var smtp = new SmtpClient())
+            try
             {
-                var email = new MimeMessage();
-                email.From.Add(MailboxAddress.Parse(configuration["SMTP_USERNAME"]));
-                email.To.Add(MailboxAddress.Parse(sendTo));
-                email.Subject = subject;
-                email.Body = new TextPart(TextFormat.Html) { Text = htmlMessage };
+                using (var smtp = new SmtpClient())
+                {
+                    var email = new MimeMessage();
+                    email.From.Add(MailboxAddress.Parse(configuration["SMTP_USERNAME"]));
+                    email.To.Add(MailboxAddress.Parse(sendTo));
+                    email.Subject = subject;
+                    email.Body = new TextPart(TextFormat.Html) { Text = htmlMessage };
 
-                await smtp.ConnectAsync(configuration["SMTP_HOST"], int.Parse(configuration["SMTP_PORT"]));
-                await smtp.AuthenticateAsync(configuration["SMTP_USERNAME"], configuration["SMTP_PASSWORD"]);
-                await smtp.SendAsync(email);
-                await smtp.DisconnectAsync(true);
+                    await smtp.ConnectAsync(configuration["SMTP_HOST"], int.Parse(configuration["SMTP_PORT"]));
+                    await smtp.AuthenticateAsync(configuration["SMTP_USERNAME"], configuration["SMTP_PASSWORD"]);
+                    await smtp.SendAsync(email);
+                    await smtp.DisconnectAsync(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogWarning($"There was an error sending email to {sendTo} with subject {subject}");
+                logger.LogError(ex, ex.Message);
             }
         }
     }
