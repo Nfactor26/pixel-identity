3f2c2d99bda226fd5a2711295144bbbe62e8d892
Nish26 <Nfactor26@gmail.com>
2022-04-16 00:13:36 +0530

    Upgrade nuget package dependencies

--- src/Pixel.Identity.Store.Mongo/AutoMapProfile.cs
+++ src/Pixel.Identity.Store.Mongo/AutoMapProfile.cs
@@ -4,39 +4,39 @@ using OpenIddict.MongoDb.Models;
 using Pixel.Identity.Shared.ViewModels;
 using Pixel.Identity.Store.Mongo.Models;
 
-namespace Pixel.Identity.Store.Mongo
+namespace Pixel.Identity.Store.Mongo;
+
+/// <summary>
+/// AutoMap profile for mapping required for MongoDb models and pixel identity view models
+/// </summary>
+public class AutoMapProfile : Profile
 {
-    /// <summary>
-    /// AutoMap profile for mapping required for MongoDb models and pixel identity view models
-    /// </summary>
-    public class AutoMapProfile : Profile
+    public AutoMapProfile()
     {
-        public AutoMapProfile()
-        {
-            CreateMap<ApplicationViewModel, OpenIddictApplicationDescriptor>()
-               .ForMember(a => a.DisplayNames, opt => opt.Ignore());
+        CreateMap<ApplicationViewModel, OpenIddictApplicationDescriptor>()
+           .ForMember(a => a.DisplayNames, opt => opt.Ignore())
+           .ForMember(a => a.Properties, opt => opt.Ignore());
 
-            CreateMap<OpenIddictMongoDbApplication, ApplicationViewModel>()
-            .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id.ToString()))
-            .ForMember(d => d.IsConfidentialClient, opt => opt.Ignore());
+        CreateMap<OpenIddictMongoDbApplication, ApplicationViewModel>()
+        .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id.ToString()))
+        .ForMember(d => d.IsConfidentialClient, opt => opt.Ignore());
 
-            CreateMap<ApplicationUser, UserDetailsViewModel>()
-             .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id.ToString()))
-             .ForMember(d => d.UserRoles, opt => opt.Ignore())
-             .ForMember(d => d.UserClaims, opt => opt.Ignore());
+        CreateMap<ApplicationUser, UserDetailsViewModel>()
+         .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id.ToString()))
+         .ForMember(d => d.UserRoles, opt => opt.Ignore())
+         .ForMember(d => d.UserClaims, opt => opt.Ignore());
 
-            CreateMap<ApplicationRole, UserRoleViewModel>()
-            .ForMember(d => d.RoleId, opt => opt.MapFrom(s => s.Id))
-            .ForMember(d => d.RoleName, opt => opt.MapFrom(s => s.Name))
-            .ForMember(d => d.Claims, opt => opt.Ignore());
+        CreateMap<ApplicationRole, UserRoleViewModel>()
+        .ForMember(d => d.RoleId, opt => opt.MapFrom(s => s.Id))
+        .ForMember(d => d.RoleName, opt => opt.MapFrom(s => s.Name))
+        .ForMember(d => d.Claims, opt => opt.Ignore());
 
-            CreateMap<ScopeViewModel, OpenIddictScopeDescriptor>()
-            .ForMember(d => d.DisplayNames, opt => opt.Ignore())
-            .ForMember(d => d.Properties, opt => opt.Ignore())
-            .ForMember(d => d.Descriptions, opt => opt.Ignore());
+        CreateMap<ScopeViewModel, OpenIddictScopeDescriptor>()
+        .ForMember(d => d.DisplayNames, opt => opt.Ignore())
+        .ForMember(d => d.Properties, opt => opt.Ignore())
+        .ForMember(d => d.Descriptions, opt => opt.Ignore());
 
-            CreateMap<OpenIddictMongoDbScope, ScopeViewModel>()
-            .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id.ToString()));
-        }
+        CreateMap<OpenIddictMongoDbScope, ScopeViewModel>()
+        .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id.ToString()));
     }
 }
