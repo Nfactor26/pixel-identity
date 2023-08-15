478d104d0e38ef69b037691d5d91708d4ab6e818
Nish26 <Nfactor26@gmail.com>
2023-08-16 00:28:54 +0530

    Github external provider demo
    Added sample plugin for exteral provider with github

---
+++ src/Pixel.Identity.External.Github/GithubProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pixel.Identity.Core;
+
+namespace Pixel.Identity.External.Github
+{
+    public class GithubProvider : IExternalAuthProvider
+    {
+        public AuthenticationBuilder AddProvider(IConfiguration configuration, AuthenticationBuilder authenticationBuilder)
+        {
+            return authenticationBuilder.AddGitHub(options =>
+            {
+                options.ClientId = "****";
+                options.ClientSecret = "***";
+                options.Scope.Add("user:email");
+            });
+        }
+    }
+}
