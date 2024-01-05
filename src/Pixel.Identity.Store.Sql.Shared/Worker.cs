b9e410ca8f5436a63b8d930b6bbcd2f89fad84a4
Doug Wilson <d@dkw.io>
2024-01-05 08:02:05 -0800

    Upgrade projects to net8.0 ...
    ... including code and project changes as required

--- src/Pixel.Identity.Store.Sql.Shared/Worker.cs
+++ src/Pixel.Identity.Store.Sql.Shared/Worker.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using OpenIddict.Abstractions;
 using OpenIddict.EntityFrameworkCore.Models;
