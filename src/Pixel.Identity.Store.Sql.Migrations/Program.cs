f00a112b8acd2a854f49022392048ff3a8917e1f
Nish26 <Nfactor26@gmail.com>
2024-01-20 12:22:45 +0530

    Add ef migrations for sql databases
    OpenIdDict 5.0 introduces some model changes. Hence, we need ef migrations to make changes to the database. Version 2 of pixel identity did not use migrations and auto created database. Starting next version, pixel-identity will require that database tables are generated first by running migration sqls.

---
+++ src/Pixel.Identity.Store.Sql.Migrations/Program.cs
@@ -0,0 +1,13 @@
+namespace Pixel.Identity.Store.Sql.Migrations;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+}
